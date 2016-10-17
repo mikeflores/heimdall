1ddd2b5e4f82e8892f59409950ad25330d9ae4d7
Mike Flores <michaelprflores@gmail.com>
2016-10-17 14:11:00 -0400

    Projects edit works

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,19 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
   end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    @project.update(project_params)
+    redirect_to projects_path(@project)
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:name)
+  end
 end
