8d291ec548ce7dd0704eb81c295ab57574eb0e85
Mike Flores <michaelprflores@gmail.com>
2016-10-17 14:46:30 -0400

    Projects add, create, new works

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,6 +3,17 @@ class ProjectsController < ApplicationController
     @projects = Project.all
   end
 
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    @project.total_hours = 0
+    @project.save
+    redirect_to projects_path(@project)
+  end
+
   def edit
     @project = Project.find(params[:id])
   end
